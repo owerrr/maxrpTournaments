b346e801b03896f944321fb05307bbc2839350cc
Prorog <proroog@gmail.com>
2021-02-15 23:50:03 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', () => {
+    document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+    if(window.pageYOffset >= 1){
+        document.getElementById("nav-menu").style.backgroundColor = "#000000D2";
+        document.getElementById("nav-menu").style.padding = "0px 0px";
+    }else{
+        document.getElementById("nav-menu").style.backgroundColor = "#000000A2";
+        document.getElementById("nav-menu").style.padding = "15px 0px"
+    }
+  }, false);
